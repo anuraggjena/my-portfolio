adb08d08c215b441f8a0ddb9b5cb199f9aa11223
Anurag Jena <anuragjena14@gmail.com>
2026-03-03 23:33:12 +0530

    update logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "../components/Header";
-import { ThemeProvider } from "next-themes";
 
 const inter = Inter({
   subsets: ["latin"],
